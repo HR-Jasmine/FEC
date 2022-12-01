8611a7e3db96d2d2c4237adc11e1bb8b6574b690
Julian McLendon <jkmclendon@gmail.com>
2022-12-01 17:32:21 -0500

    Incorporated average rating

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -10,11 +10,14 @@ const Reviews = ({productId}) => {
   const [allReviews, setAllReviews] = useState([]);
   const [numOfRevs, setNumOfRevs] = useState(2);
   const [displayReviews, setDisplayReviews] = useState([]);
+  const [averageRating, setAverageRating] = useState(0);
 
   useEffect(() => {
     if (productId === '') {
       return;
     }
+
+    // Get the reviews
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?product_id=${productId}`,
       {
         headers: {
@@ -25,6 +28,27 @@ const Reviews = ({productId}) => {
         setAllReviews(response.data.results);
         setDisplayReviews(response.data.results);
       })
+
+    // Get the review metaData
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta?product_id=${productId}`,
+    {
+      headers: {
+        'Authorization': process.env.API_KEY
+      }
+  })
+    .then(response => {
+      let reviewAgg = response.data.ratings;
+      let numOfRatings = 0;
+      let totalStars = 0;
+
+      for (var key in reviewAgg) {
+        numOfRatings += Number(reviewAgg[key]);
+        totalStars += reviewAgg[key] * key;
+      }
+
+      setAverageRating(totalStars / numOfRatings);
+    })
+
   }, [productId])
 
   const sortSelector = (param) => {
@@ -47,10 +71,16 @@ const Reviews = ({productId}) => {
     return (
       <div className="reviews-panel">
         <div className="review-breakdown">
-          <Stars rating="5" />
+          <p className="average-rating-text">Average rating: {Math.floor(averageRating * 100) / 100} stars</p>
+          <Stars rating={averageRating} />
         </div>
         <div className="review-nav">
           Sort by: &nbsp;&nbsp;
+          <select className="sort-menu" onChange={(e) => {sortSelector(e.target.value)}}>
+            <option value="relevance">Relevance</option>
+            <option value="helpful">Helpful</option>
+            <option value="newest">Newest</option>
+          </select>
         </div>
         <div className="review-list">
           {displayReviews.slice(0, numOfRevs).map((review, i) => {
