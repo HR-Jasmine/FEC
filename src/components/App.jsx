052476ac9dbef6b31524f7d257152d7d41a43f43
Kong Yu Chen <kychen249@gmail.com>
2022-12-01 10:33:12 -0500

    post merge of overview and stars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-
+import QA from './Kong/Main.jsx'
 import Reviews from './Reviews/Reviews.jsx';
 import Overview from './Overview/Overview.jsx';
 import './styles/app.css';
@@ -31,8 +31,9 @@ const App = () => {
 
   return (
     <div className="app">
-      <QA/>
+
       <Overview product={state.product} />
+      <QA/>
       <Reviews  productId={state.productId} />
     </div>
   )
