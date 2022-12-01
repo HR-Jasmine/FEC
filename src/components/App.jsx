3d3aee2a5812fdec1f8a30883bd5a607c004b4bb
ryananger <nightcrawling@gmail.com>
2022-12-01 09:00:10 -0500

    Refactored axios GET into function getProducts.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,27 @@ import Reviews from './Reviews/Reviews.jsx';
 import Overview from './Overview/Overview.jsx';
 import './styles/app.css';
 
+var headers = {'Authorization': process.env.API_KEY};
+
 const App = () => {
   const [state, setState] = useState({
     productId: '1',
     product: null
   });
 
-  useEffect(() => {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/',
-      {
-        headers: {
-          'Authorization': process.env.API_KEY
-        }
-    })
+  var getProducts = function() {
+    var url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/';
+
+    axios.get(url, {headers})
       .then(response => {
-        setState({productId: response.data[0].id, product: response.data[0]});
+        setState({
+          productId: response.data[0].id,
+          product: response.data[0]
+        });
       })
-  }, [])
+  };
+
+  useEffect(getProducts, [])
 
   return (
     <div className="app">
