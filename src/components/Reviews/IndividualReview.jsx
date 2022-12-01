2d3ef723bde4e4320fe7b93e1f41185e91d8ad9b
Julian McLendon <jkmclendon@gmail.com>
2022-12-01 11:09:41 -0500

    Response from seller added

--- src/components/Reviews/IndividualReview.jsx
+++ src/components/Reviews/IndividualReview.jsx
@@ -14,17 +14,22 @@ const IndividualReview = ({review}) => {
     <div className="review-item">
       <h4 className="review-title">{review.summary}</h4>
       <Stars rating={review.rating} />
-      <h5>{format(parseISO(review.date), 'MMMM dd, yyyy')}</h5>
-      <p>
+      <h6 className="review-username">Review by: {review.reviewer_name}</h6>
+      <h5 className="review-date">{format(parseISO(review.date), 'MMMM dd, yyyy')}</h5>
+      <p className="review-body">
         {reviewBody}<br></br>
-        {review.photos.map(photo => {
-          return <img src={photo.url} width="150"></img>
+        {review.photos.map((photo, i) => {
+          return <img src={photo.url} key={i} width="150"></img>
         })}<br></br>
         {review.recommend ? <a>I recommend this product &#x2713;</a> : null}
         <button hidden={review.body.length >= 250 ? false : true} onClick={(e) => {
           e.target.preventDefault();
           setReviewBody(review.body);
         }}>See More</button>
+      </p>
+      <p className="review-response" hidden={review.response === null ? true : false}>
+        Response from seller:<br></br>
+        {review.response}
       </p>
     </div>
   )
