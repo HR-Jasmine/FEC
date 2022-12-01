4c25269f902f9a0059585d0e4f79b4d6cb60dd47
Julian McLendon <jkmclendon@gmail.com>
2022-12-01 10:35:03 -0500

    Recommend this product completed

--- src/components/Reviews/IndividualReview.jsx
+++ src/components/Reviews/IndividualReview.jsx
@@ -6,6 +6,8 @@ import '../styles/Reviews/individual-review.css';
 
 const IndividualReview = ({review}) => {
 
+  console.log(review);
+
   const [reviewBody, setReviewBody] = useState(review.body.slice(0, 250));
 
   return (
@@ -14,7 +16,11 @@ const IndividualReview = ({review}) => {
       <Stars rating={review.rating} />
       <h5>{format(parseISO(review.date), 'MMMM dd, yyyy')}</h5>
       <p>
-        {reviewBody}
+        {reviewBody}<br></br>
+        {review.photos.map(photo => {
+          return <img src={photo.url} width="150"></img>
+        })}<br></br>
+        {review.recommend ? <a>I recommend this product &#x2713;</a> : null}
         <button hidden={review.body.length >= 250 ? false : true} onClick={(e) => {
           e.target.preventDefault();
           setReviewBody(review.body);
